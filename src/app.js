590dedf2605eb549d6263e8d91f6e383efcc0775
vini <vinicius.brandaot@gmail.com>
2023-01-14 23:33:10 -0300

    mongo not connecting

--- src/app.js
+++ src/app.js
@@ -2,21 +2,37 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb";
 
-const mongoClient = new MongoClient("mongodb://127.0.0.1:27017")
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017")
+
+let db;
 
 mongoClient.connect()
-    .then(()=>{
-        db = mongoClient.db("test")
-    }
-    )
-    .catch(()=> console.log("Monogodb Offline"))
-    
-    const users = [];
-    
-const PORT = 5000
+.then(()=>{db = mongoClient.db("test")})
+.catch(()=> {console.log("Monogodb Offline")})
 
-app.listen(PORT, () => console.log('server online'))
 
+    app.get("/participants", (req, res) => {
+        db.collection("test").find().toArray().then(participants => {
+            console.log(participants);
+            return res.send(participants)
+        }).catch(() => console.log('Data server error!'));
+    });
+    
+    app.post("/participants", (req, res) => {
+        const { name } = req.body
+
+        db.collection("test").insertOne({name : 'example'})
+            
+        })
+            
+            console.log(db)
+            
+            
+            const PORT = 5000;
+            app.listen(PORT, () => console.log('server online'))
+            
+            
+            
