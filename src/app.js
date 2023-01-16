fac055ab413d11614d31bf2fc045fe1e66b543be
teuprimo <vinicius.brandaot@gmail.com>
2023-01-16 14:24:36 -0300

    422 post codes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
-import Joi from "joi";
+import joi from "joi";
 import dayjs from "dayjs";
 
 dotenv.config()
@@ -14,9 +14,13 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 console.log(process.env.DATABASE_URL)
 let db;
 
-mongoClient.connect()
-    .then(()=>{db = mongoClient.db(), console.log("Mongo Online")})
-    .catch(()=> {console.log("Monogodb Offline")})
+try{
+    await mongoClient.connect()
+    db = mongoClient.db()
+    console.log("Mongo Online")
+}catch(error){
+        console.log("Monogodb Offline")
+}
 
 app.post("/participants", async (req, res) => {
     
@@ -28,13 +32,24 @@ app.post("/participants", async (req, res) => {
     const logTime = dayjs(timestamp).format("HH:mm:ss");
     console.log(name)
     
+    const participantSchema = joi.object({
+        name: joi.string().required(),
+    });
+
+    const validation = participantSchema.validate({ name });
+
+    if (validation.error) {
+        const err = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(err);
+    }
+
     if(!name){return res.sendStatus(422)}
 
     if(userIsLogged){ console.log(userIsLogged);return res.sendStatus(409) }
 
     await db.collection("participants").insertOne({"name": name, "lastStatus": Date.now()})
 
-    await db.collection("test").insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: logTime })
+    await db.collection("messages").insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: logTime })
 
     return res.sendStatus(201)
     }
@@ -54,10 +69,27 @@ app.get("/participants", (req, res) => {
 
 app.post("/messages", async (req, res) => {
     try{
-        
-        const { to , text, type } = req.body  
+        const { to , text, type } = req.body
+        const user = req.headers.user;
         const timestamp = Date.now()
         const msgTime = dayjs(timestamp).format("HH:mm:ss");    
+
+        const messageScheme = joi.object({
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.string().valid("message").valid("private_message").required(),
+            from: joi.string().required()
+        });
+        
+        const validation = messageScheme.validate(
+            { to, text, type, from: user },
+            { abortEarly: false }
+        );
+
+        if (validation.error) {
+            const err = validation.error.details.map((detail) => detail.message);
+            return res.status(422).send(err);
+        }
         
         if(!to || !text || !type){return res.sendStatus(422)}
     
@@ -71,6 +103,10 @@ app.post("/messages", async (req, res) => {
 })
 
 app.get("/messages", (req, res) => {
+
+    // const { limit } = req.query;
+    // const user = req.headers.user;
+
     db.collection("messages").find().toArray().then(messages => {
         return res.send(messages)
     }).catch(() => console.log('Data server error!'));
