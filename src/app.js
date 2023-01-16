8418f39c65f07ca057abbf1672622a2241b9cb85
teuprimo <vinicius.brandaot@gmail.com>
2023-01-15 21:22:53 -0300

    Add name

--- src/app.js
+++ src/app.js
@@ -4,31 +4,30 @@ import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 const mongoClient = new MongoClient("mongodb://127.0.0.1:27017")
 
 let db;
 
 mongoClient.connect()
-.then(()=>{db = mongoClient.db("test")})
+.then(()=>{db = mongoClient.db("test"), console.log("Mongo Online")})
 .catch(()=> {console.log("Monogodb Offline")})
 
+app.post("/participants", (req, res) => {
+    const { name , id} = req.body
+    console.log(req.body)
+    
+    db.collection("test").insertOne({"name": name}).then(() => {return res.sendStatus(201)}).catch(() => {return res.sendStatus(422)})
+    // return res.status(200).send('deu bom')
+    })
 
     app.get("/participants", (req, res) => {
         db.collection("test").find().toArray().then(participants => {
-            console.log(participants);
             return res.send(participants)
         }).catch(() => console.log('Data server error!'));
     });
     
-    app.post("/participants", (req, res) => {
-        const { name } = req.body
-
-        db.collection("test").insertOne(name)
             
-        })
-            
-            console.log(db)
             
             
             const PORT = 5000;
